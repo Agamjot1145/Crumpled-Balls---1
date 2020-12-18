9a046dedb7ae6e0d1447cdfb2edd30e8cc92fbe1
Agamjot1145 <74133529+Agamjot1145@users.noreply.github.com>
2020-12-18 12:02:11 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/Paper.js
@@ -0,0 +1,22 @@
+class Paper{
+    constructor(x,y,r){
+        this.image = loadImage("sprites/crumpld.jpg");
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.Body=Bodies.circle(x,y,r,options)
+        World.add(world,this.Body);
+        this.r=r
+    }
+    display(){
+        var pos=this.Body.position
+        push()
+        translate(pos.x,pos.y)
+        ellipseMode(RADIUS)
+        ellipse(0,0,this.r,this.r)
+        pop()
+    }
+}
